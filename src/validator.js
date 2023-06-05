ab8fb9ba978c16b9f88a5ff1dd5f4e0cce33a205
CAMIANAIS <cmamanicr@unsa.edu.pe>
2023-06-05 11:13:54 -0500

    html y css

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
   // ...
+
+isValid :(NumerodeTarjeta)
+let LuhnSum=0
+
+  NumerodeTarjeta = NumerodeTarjeta.replace(" ","")
+  
+  if (isValid(NumerodeTarjeta)){
+    console.log("Numero de Tarjeta Valida")
+    else
+    console.log("Numero de Tarjeta Invalida")
+  }
+
+  validator.maskify(creditCardNumber): creditCardNumber
 };
 
 export default validator;

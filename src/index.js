9b8b944d79357c5a42d7e2e35d4a4a99f9d0b063
CAMIANAIS <cmamanicr@unsa.edu.pe>
2023-06-09 22:53:43 -0500

    corren los test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import validator from './validator.js';
 
-console.log(validator);
 
+const tarjeta = document.getElementById('tarjeta');
+const formulario = document.getElementById('formCard');
+
+
+formulario.addEventListener("submit",function(e){
+  e.preventDefault();
+  let valueCard=tarjeta.value;
+  console.log(valueCard)
+  console.log(validator.isValid(valueCard))
+  if (validator.isValid(valueCard)) {
+    valueCard = "Tarjeta de Crédito Válida";
+  } else {
+    valueCard = "Tarjeta de Crédito Inválida";
+  }
+  document.getElementById("validez").innerHTML = valueCard;
+})
+
+formulario.addEventListener("click", function(e){
+  e.preventDefault();
+  const valueCard = tarjeta.value;
+  console.log(validator.maskify(valueCard));
+
+})
 
--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,38 @@
 const validator = {
   // ...
+  maskify:function(CardNumber){
+    const length = CardNumber.length;
+    const lastFourDigits = CardNumber.slice(-4);
+    const maskedDigits = length - 4 < 0 ? '' : '#'.repeat(length - 4);
 
+    return maskedDigits + lastFourDigits;},
+
+  
   isValid:function(CardNumber){
-    let reverseNumber=CardNumber.split('').reverse();
+    
+    const reverseNumber=CardNumber.split('').reverse();
+    
+    let sum=0;
+    let doubleDigits=false;
 
-  
-    return reverseNumber
+    for(let i=0;i<reverseNumber.length;i++){
+      let digit=parseInt(reverseNumber[i],10)
 
+      if (doubleDigits) {
+        digit *=2;
+        if(digit>9){
+          digit=digit-9;
+        }
+      }
+      sum+=digit;
+      doubleDigits=!doubleDigits
+    }
+    return sum%10===0;
+    
+      
   }
-  
+
 }
 
 export default validator;
+
